94e248679aa656d7fff46227b0faa9a636b5613b
William Merfalen <wmerfalen@gmail.com>
2022-11-04 01:01:11 +0000

    feat: filter by first/last name

--- src/pages/Members.js
+++ src/pages/Members.js
@@ -9,31 +9,38 @@ function profile_link(user) {
 function Members() {
   const [members, setMembers] = useState(null);
   const [error, setError] = useState(null);
+  const search_box = useRef();
 
-  useEffect(() => {
+  function fetch_members() {
     fetch("https://randomuser.me/api/?page=1&results=20&seed=randomuser")
       .then((res) => res.json())
       .then(
         (result) => {
           let page = [];
           for (const user of result.results) {
-            page.push(
-              <a href={profile_link(user)}>
-                <div className="profile-preview">
-                  <div className="profile-thumb-wrapper">
-                    <img
-                      className="profile-thumbnail"
-                      src={user.picture.thumbnail}
-                    />
+            if (
+              search_box.current.value === undefined ||
+              user.name.first.match(search_box.current.value) ||
+              user.name.last.match(search_box.current.value)
+            ) {
+              page.push(
+                <a href={profile_link(user)}>
+                  <div className="profile-preview">
+                    <div className="profile-thumb-wrapper">
+                      <img
+                        className="profile-thumbnail"
+                        src={user.picture.thumbnail}
+                      />
+                    </div>
+                    <div className="profile-name-wrapper">
+                      <b>
+                        {user.name.first} {user.name.last}
+                      </b>
+                    </div>
                   </div>
-                  <div className="profile-name-wrapper">
-                    <b>
-                      {user.name.first} {user.name.last}
-                    </b>
-                  </div>
-                </div>
-              </a>
-            );
+                </a>
+              );
+            }
           }
           if (page.length === 0) {
             setError("Unable to fetch results from server");
@@ -48,12 +55,27 @@ function Members() {
           setMembers([]);
         }
       );
+  }
+  function search() {
+    fetch_members();
+  }
+
+  useEffect(() => {
+    fetch_members();
   }, []);
 
   return (
     <div>
       <div className="nav">
         <Link to="/">Home</Link>
+        <div className="search">
+          <input
+            type="text"
+            onKeyUp={() => search()}
+            ref={search_box}
+            placeholder="Filter (case sensitive)"
+          />
+        </div>
         <div className="settings">
           <img src={settings} />
         </div>
